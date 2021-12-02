f4f39458d33fbe148a7d8de116c884fbea4d5332
sherlockvn <quangkhai28999@gmail.com>
2021-12-02 09:45:58 +0700

    add comments

--- base_model.py
+++ base_model.py
@@ -6,20 +6,27 @@ from keras.models import Model
 from config import *
 
 def create_base_model():
-  #base model
+  #use light weight deep neutral networks 
   base_model = MobileNet(
       weights= None, 
       include_top=False, 
       input_shape= (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
   )
-
+  # get one output tensor of the layer
   x = base_model.output
+  # apply global average pooling operation to output tensor to reduce the dimension of data
   x = GlobalAveragePooling2D()(x)
+  # add fully connected layer with 256 units and the rectified linear unit(relu) activation function.
   x = Dense(256,activation='relu')(x) 
+  # add dropout layer which set input units to 0 with a frequency of rate = 0.2 for avoiding overfitting
   x = Dropout(0.2)(x)
+  # last layer is fully connected layer with number of unit equals to number of class, use softmax activation
+  # because softmax output is probability distribution of vector inputs
   predictions = Dense(NUM_CLASSES, activation='softmax')(x)
   model = Model(inputs=base_model.input, outputs=predictions)
-
+  # use Adam optimizer with learing rate is 0.000125
   opt = adam.Adam(lr=0.000125)
+  # configure model with loss function is categorial cross entropy because of multi-class classfication task
+  # and metric is accuracy only before start training 
   model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
   return model
--- create_dataset.py
+++ create_dataset.py
@@ -6,22 +6,25 @@ import pandas as pd
 # create dataset
 def create_dataset():
   dataset = []
+  # get all files in each image directories, then push it to dataset
   for fold in os.listdir(IMG_DIR):
       for filename in os.listdir(f'{IMG_DIR}/{fold}'):
           dataset.append((f'{fold}/{filename}', fold))
-
+  # convert dataset to tabular form with column are filename, category
   df = pd.DataFrame(dataset, columns=['filename', 'category'])
+  # split dataset into 2 set: training set and testint set with ratio 80:20
   df_train, df_test = train_test_split(df, random_state=42, stratify=df.category, test_size=.2)
   df_train['set'] = 'train'
   df_test['set'] = 'test'
   df = df_train.append(df_test)
+  # convert data to csv file
   df.to_csv('dataset.csv', index=False)
   df.head()
 
   #read and prepare data
   df = pd.read_csv('dataset.csv')
   df.head()
-  
+  # generate new dataframe with index reset
   train_df = df[df.set == 'train'].reset_index(drop=True)
   validate_df = df[df.set == 'test'].reset_index(drop=True)
   return [train_df, validate_df]
